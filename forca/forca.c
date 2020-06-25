d95bf3c884823716ef4af6284a0a3beefddd9dc7
Roque Laecio dos Santos <rlaecio@hotmail.com>
2020-06-25 02:12:18 +0100

    new functions

--- forca/forca.c
+++ forca/forca.c
@@ -30,11 +30,33 @@ int jachutou(char letra, char chutes[26], int tentativas)
     return achou;
 }
 
+void desenhaforca(char palavrasecreta[20], char chutes[26], int tentativas)
+{
+    // imprime a palavra secreta
+    for (int i = 0; i < strlen(palavrasecreta); i++)
+    {
+        int achou = jachutou(palavrasecreta[i], chutes, tentativas);
+
+        if (achou)
+        {
+            printf("%c ", palavrasecreta[i]);
+        }
+        else
+        {
+            printf("_ ");
+        }
+    }
+    printf("\n");
+}
+
+void escolhepalavra(char palavrasecreta[20])
+{
+    sprintf(palavrasecreta, "MELANCIA");
+}
+
 int main()
 {
-
     char palavrasecreta[20];
-    sprintf(palavrasecreta, "MELANCIA");
 
     int acertou = 0;
     int enforcou = 0;
@@ -42,28 +64,13 @@ int main()
     char chutes[26];
     int tentativas = 0;
 
-    // abertura
+    escolhepalavra(palavrasecreta);
+
     abertura();
 
     do
     {
-        // imprime a palavra secreta
-        for (int i = 0; i < strlen(palavrasecreta); i++)
-        {
-            int achou = jachutou(palavrasecreta[i], chutes, tentativas);
-
-            if (achou)
-            {
-                printf("%c ", palavrasecreta[i]);
-            }
-            else
-            {
-                printf("_ ");
-            }
-        }
-        printf("\n");
-
+        desenhaforca(palavrasecreta, chutes, tentativas);
         chuta(chutes, &tentativas);
-
     } while (!acertou && !enforcou);
 }
